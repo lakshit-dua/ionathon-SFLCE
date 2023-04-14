32cf75a04a3f7db14d77805b7949611ba05fb0d5
chirag.khanna <chirag94.ck@gmail.com>
2023-04-14 23:02:39 +0530

    directory functionality

--- server/directorySearchHandler.js
+++ server/directorySearchHandler.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const Zip = require('node-7z-forall');
+const path = require('path');
 const baseDir = "./public/logs"
 
 class DirectorySearchHandler {
@@ -10,6 +12,7 @@ class DirectorySearchHandler {
 
     init() {
         this.socket.on('getPublicDir', (location) => this.handlePublicDirQuery(location));
+        this.socket.on('getArchiveList', (location) => this.handleArchiveListRequest(location));
     }
 
     handlePublicDirQuery(location) {
@@ -29,6 +32,21 @@ class DirectorySearchHandler {
             console.log(err)
             self.connector.sendMessage({err});
         }
+    }
+
+    handleArchiveListRequest(location) {
+        const path2 = path.join(__dirname, baseDir+"/"+location);
+        let entries = [];
+        console.log(path2);
+        Zip.list(path2).progress((entry) => {
+            entries = entries.concat(entry);
+        }).then(() =>{
+            console.log(entries);
+            this.connector.sendMessage({entries});
+        }).catch(err => {
+            console.log(err);
+            this.connector.sendMessage({err});
+        });
     }
 }
 
